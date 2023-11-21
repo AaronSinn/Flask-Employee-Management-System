7e89215b87cf8f5042303c58e0423ab2580c5f38
Aaron Sinn <aaron.sinn@me.com>
2023-11-20 23:30:58 -0500

    restrctured files and added __init__.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,7 @@
-#TODO restrcture and make this a run.py maybe
-from flask import Flask, render_template, url_for
-from forms import LoginForm, RegisterForm
+from flask import render_template, url_for
+from app.forms import LoginForm, RegisterForm
+from app import app
 
-app = Flask(__name__)
-app.secret_key = "RickyDickyDooDahGrimes"
 
 @app.route('/')
 def home():
@@ -21,6 +19,3 @@ def register():
     form = RegisterForm()
 
     return render_template('register.html', form=form)
-
-if __name__ == "__main__":
-    app.run(debug=True)
---
+++ run.py
@@ -0,0 +1,4 @@
+from app import app #imports from init
+
+if __name__ == "__main__":
+    app.run(debug=True)
